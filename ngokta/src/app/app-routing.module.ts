7c8e36d42e3b80067a61c519c0fc6962dfe47bb6
Randall Krebs <randumfaktor@gmail.com>
2017-12-10 17:52:15 -0500

    add Home and Token components for routing. protect token route with the AuthGuard.

--- ngokta/src/app/app-routing.module.ts
+++ ngokta/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './shared';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TokenComponent } from './token/token.component';
+
+const routes: Routes = [
+  {path: 'token', component: TokenComponent, canActivate: [AuthGuard]},
+  {path: 'home', redirectTo: ''},
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers: [AuthGuard],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ngokta/src/app/app.module.ts
+++ ngokta/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TokenComponent } from './token/token.component';
+
+import { AuthGuard } from './shared';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TokenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
