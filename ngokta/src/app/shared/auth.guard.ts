d037a9e77588ca29da691c0c72cd4f4f9f567a7d
Randall Krebs <rkrebs@mimeo.com>
2018-02-23 17:34:19 -0500

    display tokens when logged in

--- ngokta/src/app/shared/auth.guard.ts
+++ ngokta/src/app/shared/auth.guard.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
+  constructor(private oauthService: OAuthService) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.oauthService.hasValidAccessToken();
   }
 }
