025fdd27769d20667b435ca44f05f0134ef9a844
Randall Krebs <rkrebs@mimeo.com>
2018-02-26 19:03:54 -0500

    delete a value, but only if you own it

--- ngokta/src/app/home/home.component.ts
+++ ngokta/src/app/home/home.component.ts
@@ -21,6 +21,13 @@ export class HomeComponent implements OnInit {
   constructor(private oauthService: OAuthService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.oauthService.events.subscribe(e => {
+      console.debug('oauth/oidc event', e);
+      if (e.type == 'token_received') {
+        this.getValues();
+      }
+    });
+
     this.getValues();
   }
 
@@ -28,15 +35,21 @@ export class HomeComponent implements OnInit {
     return this.oauthService.hasValidAccessToken(); 
   }
 
+  public get ownerEmail(): string {
+    let claims = this.oauthService.getIdentityClaims();
+    if (claims['email']) {
+      return claims['email'];
+    }
+
+    return "xyzzy plugh";    // random string that does not match email address or null
+  }
+
   getValues(): void {
     this.values = [];
-
-    let headers = new HttpHeaders();
-    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
-    headers.append('Accept', 'application/json');
+    this.errorMessage = null;
 
     // return observable
-    let webObservable = this.http.get(this.valuesUrl, { headers: headers });
+    let webObservable = this.http.get(this.valuesUrl, { headers: this.jsonHeaders() });
 
     webObservable.flatMap((x: any) => x).subscribe(
       (v) => {
@@ -49,20 +62,33 @@ export class HomeComponent implements OnInit {
   }
 
   addNewValue(): void {
-    let headers = new HttpHeaders();
-    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
-    headers.append('Accept', 'application/json');
-
     // post new value
-    let webObservable = this.http.post(this.valuesUrl, '"' + this.newValue + '"', { headers: headers });
+    let webObservable = this.http.post(this.valuesUrl, '"' + this.newValue + '"', { headers: this.jsonHeaders() });
+    this.handleResponse(webObservable);
+  }
 
-    webObservable.subscribe(
+  deleteValue(valu: string): void {
+    let webObservable = this.http.delete(this.valuesUrl + '/' + valu, { headers: this.jsonHeaders() });
+    this.handleResponse(webObservable);
+  }
+
+  handleResponse(obs: Observable<object>): void {
+    obs.subscribe(
       (v) => {
         this.newValue = null;
+        this.getValues();
       },
       (err) => {
         this.errorMessage = err.message;
         console.log(JSON.stringify(err));
       });
+  }
+
+  jsonHeaders(): HttpHeaders {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    headers.append('Accept', 'application/json');
+
+    return headers;
   }
 }
